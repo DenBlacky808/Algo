9809a708a281ab25b0968ad243c2fdd6b1a87a22
DenisChernyavskiy <92929@mail.ru>
2021-03-05 17:34:12 +0300

    Lesson_3

---
+++ Lesson_3/Task_5.py
@@ -0,0 +1,15 @@
+import random
+
+SIZE = 10
+MIN_ITEM = -100
+MAX_ITEM = 1
+array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+print(array)
+
+negative_item_pos = 0
+negative_item = MIN_ITEM
+for i in range(len(array)):
+    if array[i] < 0 and abs(array[i]) < abs(negative_item):
+        negative_item_pos = i
+        negative_item = array[i]
+print(negative_item, negative_item_pos)
